d4ea34278eeb439eb51e0dae9913c01e9b8cb172
Kirill <kiria12312@mail.ru>
2025-01-27 00:19:10 +0300

    feat: add loading svg

--- src/pages/FavoritesPage.tsx
+++ src/pages/FavoritesPage.tsx
@@ -59,8 +59,61 @@ export default function FavoritesPage() {
         </div>
 
         {loading && (
-          <div className="py-24 flex justify-center items-center">
+          <div className="flex justify-center items-center">
             <p className="ml-2 text-lg">Loading... </p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 200 200"
+              className="w-7 h-7"
+            >
+              <radialGradient
+                id="a9"
+                cx="0.66"
+                fx="0.66"
+                cy="0.3125"
+                fy="0.3125"
+                gradientTransform="scale(1.5)"
+              >
+                <stop offset="0" stopColor="#000000"></stop>
+                <stop offset="0.3" stopColor="#000000" stopOpacity="0.9"></stop>
+                <stop offset="0.6" stopColor="#000000" stopOpacity="0.6"></stop>
+                <stop offset="0.8" stopColor="#000000" stopOpacity="0.3"></stop>
+                <stop offset="1" stopColor="#000000" stopOpacity="0"></stop>
+              </radialGradient>
+              <circle
+                style={{ transformOrigin: 'center' }}
+                fill="none"
+                stroke="url(#a9)"
+                strokeWidth="15"
+                strokeLinecap="round"
+                strokeDasharray="200 1000"
+                strokeDashoffset="0"
+                cx="100"
+                cy="100"
+                r="70"
+              >
+                <animateTransform
+                  type="rotate"
+                  attributeName="transform"
+                  calcMode="spline"
+                  dur="2s"
+                  values="360;0"
+                  keyTimes="0;1"
+                  keySplines="0 0 1 1"
+                  repeatCount="indefinite"
+                ></animateTransform>
+              </circle>
+              <circle
+                fill="none"
+                opacity="0.2"
+                stroke="#000000"
+                strokeWidth="15"
+                strokeLinecap="round"
+                cx="100"
+                cy="100"
+                r="70"
+              ></circle>
+            </svg>
           </div>
         )}
 
